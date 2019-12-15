8888d4b6d594132f0d2d2898204417ba21f4a60c
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-15 23:12:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("Select last_insert_rowid() From students")[0][0]
   end
-  def self.create(name:, album:)
+  def self.create(name, album)
      student = Student.new(name, grade)
      student.save
      student
