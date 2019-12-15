77ebd0922f813fc923b3a3af6e3c7779f59529d4
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-15 23:14:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,13 @@ class Student
   end
   def self.find_by_name
 
+       def self.new_from_db(row)
+        id = row[0]
+        name = row[1]
+        grade = row[2]
+        new_student = self.new(id, name, grade)
+        new_student
+      end
   end
   def update
     sql = <<-SQL
